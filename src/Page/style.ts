ef81cba87306ae5d2d4125b0b66a810ebac6d409
silmaraMoreira <silmaraa.1994@icloud.com>
2023-11-08 19:25:34 -0300

    fix: organized code

--- src/Page/style.ts
+++ src/Page/style.ts
@@ -2,15 +2,15 @@ import styled from "styled-components";
 import backgroundImage from "../assets/banner-bg.png";
 
 export const Container = styled.div`
-  height: 100vh;
-  background-image: url(${backgroundImage});
-  background-size: cover;
-  background-position: center;
-  background-attachment: fixed; 
-  z-index: 1;
-  filter: saturate(100%) contrast(150%);
+    height: 100vh;
+    background-image: url(${backgroundImage});
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed; 
+    z-index: 1;
+    filter: saturate(100%) contrast(150%);
 
-  @media (max-width: 768px) {
-    height: 50vh;
-  }
+    @media (max-width: 768px) {
+      height: 50vh;
+    }
 `;
